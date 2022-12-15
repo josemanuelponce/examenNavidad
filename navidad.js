7212425f39c62a64aa26e62140ae554d5ed3803a
josemanuelponce <josee.manuel.9700@gmail.com>
2022-12-15 17:15:41 +0100

    commit 7

--- navidad.js
+++ navidad.js
@@ -2,7 +2,7 @@
 const myTimeout = setTimeout(mostrarSaludo, 3000);
 
 function mostrarSaludo() {
-  document.getElementById("felicitar").style.visibility="visible";
+  document.getElementById("saludos").style.visibility="visible";
 }
 
 //Te muestra el texto al hacer click
@@ -10,7 +10,7 @@ let body = document.querySelector("body");
 body.addEventListener("click", ocultarSaludo);
 
 function ocultarSaludo() {
-    document.getElementById("felicitar").style.visibility="visible";
+    document.getElementById("saludos").style.visibility="visible";
 }
 
 //Te cambia de imagen cuando hace click y te muestra el texto
